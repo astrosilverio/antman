3dac8ad0915e9145095633c0f8d66f629d8b8fc5
Katie Silverio <astrosilverio@gmail.com>
2013-10-29 14:30:48 -0400

    defining a port

--- app.py
+++ app.py
@@ -7,4 +7,5 @@ def index():
 	return render_template('template.html')
 	
 if __name__ == "__main__":
-	app.run(debug=True)
+	port = int(os.environ.get('PORT',5000))
+	app.run(host='0.0.0.0', port=port)
