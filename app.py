48b81c8af36b1c424151feb4b075a051850fa648
Katie Silverio <astrosilverio@gmail.com>
2013-10-29 14:10:21 -0400

    init

---
+++ app.py
@@ -0,0 +1,10 @@
+import os
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+	return render_template('template.html')
+	
+if __name__ == "__main__":
+	app.run(debug=True)
