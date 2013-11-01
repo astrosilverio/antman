b21ae0adffe0cc5d70436f85bd643f0c6b310972
Katie Silverio <astrosilverio@gmail.com>
2013-11-01 19:57:24 -0400

    added a form and ability to deal with POST requests

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
 import os
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
-@app.route('/',methods=['GET'])
+@app.route('/',methods=['GET', 'POST'])
 def index():
-	return render_template('template.html')
-	
+	if request.method == 'GET':
+		return render_template('template.html')
+	else:
+		# request was a POST
+		url_to_shorten = request.form['url']
+		
 if __name__ == "__main__":
 	port = int(os.environ.get('PORT',5000))
 	app.run(host='0.0.0.0', port=port)
