1531e488864417729e8b9be7ec1fbfd56440f1b2
Katie Silverio <astrosilverio@gmail.com>
2013-11-01 20:29:17 -0400

    Forgot to import redirect :-/

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 app = Flask(__name__)
 
 @app.route('/index',methods=['GET', 'POST'])
